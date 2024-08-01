f60def2595e44b0d510d444a1c7d61912770ff30
FlavioPals <flaviopals@hotmail.com>
2024-08-01 14:10:06 -0300

    feat:add enviroment variables

--- src/Database/setupKnex.ts
+++ src/Database/setupKnex.ts
@@ -1,10 +1,13 @@
 import { knex as setupKnex, Knex } from 'knex';
+import 'dotenv/config'
+import { env } from '../env';
+
 
 
 export const config:Knex.Config= {
     client: 'sqlite3',
     connection: {
-        filename: './src/Database/database.db'
+        filename: env.DATABASE_URL
     },
     useNullAsDefault: true,
     migrations: {
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,29 @@
 import express from 'express'
 import { knex } from './Database/setupKnex'
+import { randomUUID } from 'crypto'
+import { env } from './env'
 
 const app = express()
 
 app.use(express.json())
 
-app.listen(3333, () =>{
+app.listen(env.PORT, () =>{
     console.log('Server is running on http://localhost:3333')
 })
 
 app.get('/',async (req, res)=> {
-    const test = await knex('sqlite_schema').select('*')
+    const test = await knex('users').select('*')
     res.json(test)
 
+})
+
+app.post('/', async(req, res)=> {
+    const test = await knex('users').insert({
+        id:randomUUID(),
+        name:'testaaa',
+        email:'test@email.com',
+        password:'1234',
+
+    }).returning('*')
+    res.json(test)
 })
